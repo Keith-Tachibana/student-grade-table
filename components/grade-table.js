7480ae697c8e0a15e347b45dc4ae2a80685fd77a
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-02-05 20:18:27 -0800

    Student Grade Table - Keith Tachibana: Finished all requirements for view all grades

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,11 +3,20 @@ class GradeTable {
     this.tableElement = tableElement;
   }
   updateGrades(grades) {
-    this.tableElement.innerHTML = '';
-    for (let i = 0; i < grades.lenth; i++) {
+    let tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+    for (let i = 0; i < grades.length; i++) {
       let tr = document.createElement('tr');
-      let td = document.createElement('td');
-
+      let td1 = document.createElement('td');
+      let td2 = document.createElement('td');
+      let td3 = document.createElement('td');
+      td1.textContent = grades[i].name;
+      td2.textContent = grades[i].course;
+      td3.textContent = grades[i].grade;
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      this.tableElement.appendChild(tr);
     }
   }
 }
