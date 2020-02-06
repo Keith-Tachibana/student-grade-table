fb699aa4bc6957f7bcfbf807d71d1b2bae3c39ec
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-02-06 01:28:28 -0800

    Student Grade Table - Keith Tachibana: Finished up to the renderGradeRow method

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,6 +1,7 @@
 class GradeTable {
-  constructor(tableElement) {
+  constructor(tableElement, noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
   updateGrades(grades) {
     let tbody = document.querySelector('tbody');
@@ -17,6 +18,32 @@ class GradeTable {
       tr.appendChild(td2);
       tr.appendChild(td3);
       this.tableElement.appendChild(tr);
+      this.renderGradeRow(grades, this.deleteGrade);
     }
+  }
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
+  }
+  renderGradeRow(data, deleteGrade) {
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+    let button = document.createElement('button');
+    button.classList.add('btn', 'btn-danger');
+    button.textContent = "DELETE";
+    td1.textContent = data.name;
+    td2.textContent = data.course;
+    td3.textContent = data.grade;
+    td4.appendChild(button);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td2);
+    tr.appendChild(td4);
+    button.addEventListener('click', function() {
+      deleteGrade(data.id);
+    });
+    this.tableElement.appendChild(tr);
   }
 }
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const table = document.querySelector('table');
 const header = document.querySelector('header');
 const form = document.querySelector('form');
+const noGrades = document.querySelector('#no-grades');
 
-const gradeTable = new GradeTable(table);
+const gradeTable = new GradeTable(table, noGrades);
 const pageHeader = new PageHeader(header);
 const gradeForm = new GradeForm(form);
 const app = new App(gradeTable, pageHeader, gradeForm);
